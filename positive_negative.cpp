d911419eceab5621a4622ef4fa793e53b1877084
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-19 23:51:56 +0500

    Create positive_negative.cpp

---
+++ positive_negative.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    if (x > 0 || x == 0) {
+        cout << "Musbat yoki nol";
+    } else {
+        cout << "Manfiy";
+    }
+}
